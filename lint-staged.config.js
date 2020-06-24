287e7aa83504b23abae3fb71ec9e91f857c8ae44
Tom Vincent <git@tlvince.com>
2020-06-24 13:51:34 +0100

    chore(repo): lint-staged now git adds

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  '*.ts': [
-    'eslint --cache --fix',
-    'git add',
-    'yarn test --bail --findRelatedTests',
-  ],
-  '*.{js,jsx,ts,tsx,json,css,md}': ['prettier --write', 'git add'],
+  '*.ts': ['eslint --cache --fix', 'yarn test --bail --findRelatedTests'],
+  '*.{js,jsx,ts,tsx,json,css,md}': ['prettier --write'],
 }
